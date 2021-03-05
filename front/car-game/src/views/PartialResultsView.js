a17a30faed473c3d62c5379ea243cb78a902d863
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-05 08:21:49 -0500

    Insert the podium once the game is over

--- front/car-game/src/views/PartialResultsView.js
+++ front/car-game/src/views/PartialResultsView.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import PodiumView from './PodiumView'
 
 const PartialResultsView = (props) => {
 
@@ -6,55 +7,66 @@ const PartialResultsView = (props) => {
 
     const [partialResults, setPartialResults] = useState([])
     const [gameStarted, setGameStarted] = useState(false);
+    const [gameEnded, setGameEnded] = useState(false);
+    const [podium, setPodium] = useState([])
 
     let runGame = async () => {
         setGameStarted(true);
         fetch(`${HOST_API}/${props.gameId}/run-game/`)
-            .then(response => response)
-            .then(response => console.log(response))
+            .then(response => response.json())
+            .then(response => {
+                setGameEnded(true);
+                setPodium(response);
+                console.log(response)
+            })
     };
 
     const bringInformation = () => {
         fetch(`${HOST_API}/partial-result/game/${props.gameId}`)
             .then(response => response.json())
-            .then(response => setPartialResults(response))
+            .then(response => {setPartialResults(response)})
     }
 
 
     useEffect(() => {
 
         setTimeout(bringInformation, 1000)
-        
-        if(!gameStarted){
+
+        if (!gameStarted) {
             runGame();
         }
 
-        
+
 
     })
 
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th scope="col">Jugador</th>
-                    <th scope="col">Color</th>
-                    <th scope="col">Distancia</th>
-                </tr>
-            </thead>
-            <tbody>
-                {partialResults.map((result) => {
-                    return (
-                        <tr key={result.playerId}>
-                            <td>{result.playerName}</td>
-                            <td>{result.color}</td>
-                            <td>{result.partialDistance}</td>
-                        </tr>
-                    )
-                })}
+        <div>
+            <table className="table" key="PartialResults">
+                <thead>
+                    <tr>
+                        <th scope="col">Jugador</th>
+                        <th scope="col">Color</th>
+                        <th scope="col">Distancia</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {partialResults.map((result) => {
+                        return (
+                            <tr key={result.playerId}>
+                                <td>{result.playerName}</td>
+                                <td>{result.color}</td>
+                                <td>{result.partialDistance}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            {gameEnded && <PodiumView podium = {podium} key="Podium"/>}
+        </div>
 
-            </tbody>
-        </table>)
+
+    )
 
 }
 
